ea621036d455d6be8c5d37f3975ba6b42cd7de84
wongpraatan <wongpratan@hotmail.com>
2022-02-23 16:50:01 +0700

    +Apply pull value of a calculate Field in the insert task - https://github.com/appdevdesigns/app_builder/commit/29b18c93 https://github.com/appdevdesigns/app_builder/commit/092e0880 https://github.com/appdevdesigns/app_builder/commit/0036fe75 https://github.com/appdevdesigns/app_builder/commit/b533d450 https://github.com/appdevdesigns/app_builder/commit/4ad3cc65

--- platform/process/tasks/ABProcessTaskServiceInsertRecord.js
+++ platform/process/tasks/ABProcessTaskServiceInsertRecord.js
@@ -19,7 +19,6 @@ module.exports = class InsertRecord extends InsertRecordTaskCore {
     *                            false if task is still waiting
     */
    do(instance, trx, req) {
-      this._req = req;
       this.object = this.AB.objectByID(this.objectID);
       if (!this.object) {
          return this.errorConfig(
@@ -46,26 +45,27 @@ module.exports = class InsertRecord extends InsertRecordTaskCore {
                pullDataTasks.push(
                   () =>
                      new Promise((next, bad) => {
-                        let PK = fieldRepeat.datasourceLink.PK();
-                        this._req
-                           .retry(() =>
-                              fieldRepeat.datasourceLink.model().findAll(
-                                 {
-                                    where: {
-                                       glue: "and",
-                                       rules: [
-                                          {
-                                             key: PK,
-                                             rule: "equals",
-                                             value: rData[PK],
-                                          },
-                                       ],
-                                    },
-                                    populate: true,
+                        fieldRepeat.datasourceLink
+                           .model()
+                           .findAll(
+                              {
+                                 where: {
+                                    glue: "and",
+                                    rules: [
+                                       {
+                                          key: fieldRepeat.datasourceLink.PK(),
+                                          rule: "equals",
+                                          value:
+                                             rData[
+                                                fieldRepeat.datasourceLink.PK()
+                                             ],
+                                       },
+                                    ],
                                  },
-                                 null,
-                                 req
-                              )
+                                 populate: true,
+                              },
+                              null,
+                              req
                            )
                            .then((result) => {
                               next(this.getDataValue(instance, result[0]));
@@ -76,7 +76,7 @@ module.exports = class InsertRecord extends InsertRecordTaskCore {
             });
          }
       }
-      // Pull a single data entry to insert
+      // Pull a data to insert
       else {
          pullDataTasks.push(() => Promise.resolve(this.getDataValue(instance)));
       }
@@ -85,28 +85,40 @@ module.exports = class InsertRecord extends InsertRecordTaskCore {
          tasks.push(
             Promise.resolve()
                .then(() => pullTask())
-               .then((val) =>
-                  this._req.retry(() => this.object.model().create(val))
-               )
+               .then((val) => this.object.model().create(val))
+               // NOTE: .create() returns the fully populated instance already.
+               // .then((record) =>
+               //    this.object.model().findAll({
+               //       where: {
+               //          glue: "and",
+               //          rules: [
+               //             {
+               //                key: this.object.PK(),
+               //                rule: "equals",
+               //                value: record[this.object.PK()],
+               //             },
+               //          ],
+               //       },
+               //       populate: true,
+               //    }, null, req)
+               // )
                .then((result) => {
                   results.push(result);
+                  return Promise.resolve();
                })
          );
       });
 
-      return Promise.all(tasks)
-         .then(() => {
-            this.stateUpdate(instance, {
-               data: results,
-            });
-            this.stateCompleted(instance);
-            return true;
-         })
-         .catch((err) => {
-            this.log(instance, "Error completing Insert Record");
-            this.onError(instance, err);
-            throw err;
-         });
+      return Promise.all(tasks).then(
+         () =>
+            new Promise((next, bad) => {
+               this.stateUpdate(instance, {
+                  data: results,
+               });
+               this.stateCompleted(instance);
+               next(true);
+            })
+      );
    }
 
    /**
@@ -116,7 +128,10 @@ module.exports = class InsertRecord extends InsertRecordTaskCore {
     * @return {mixed} | null
     */
    processData(instance, key) {
-      let myState = this.myState(instance);
+      const parts = key.split(".");
+      if (parts[0] != this.id) return null;
+
+      let myState = this.myState(instance) || {};
       let data = myState.data;
       if (data == null) return null;
 
@@ -245,7 +260,102 @@ module.exports = class InsertRecord extends InsertRecordTaskCore {
                break;
             case "4": // formula value
                if (item.value) {
-                  let evalValue = eval(item.value);
+                  let formula = item.value || "";
+
+                  // pull [PARAMETER NAME] names
+                  let paramNames = formula.match(/\[(.*?)\]/g) || [];
+                  paramNames.forEach((match) => {
+                     let param = match.replace(/\[/g, "").replace(/\]/g, "");
+
+                     let sourceName = param.split(".")[0];
+                     let fieldName = param.split(".")[1];
+
+                     // Pull data from the start trigger
+                     // or a previous insert process task
+                     if (
+                        fieldName &&
+                        (sourceName == "startData" ||
+                           sourceName == "previousData")
+                     ) {
+                        // Pull an object
+                        let sourceObj =
+                           sourceName == "startData"
+                              ? this.objectOfStartElement
+                              : this.objectOfPrevElement;
+                        if (!sourceObj) return;
+
+                        // Pull a field
+                        let sourceField = sourceObj.fields((f) => {
+                           return (
+                              f.id == fieldName ||
+                              f.columnName == fieldName ||
+                              (f.translations || []).filter(
+                                 (tran) => tran.label == fieldName
+                              ).length
+                           );
+                        })[0];
+                        if (!sourceField) return;
+
+                        // Get value from a field that calculates value on fly
+                        if (sourceField.key == "calculate") {
+                           formula = formula.replace(
+                              match,
+                              sourceName == "startData"
+                                 ? sourceField.format(startData)
+                                 : sourceField.format(previousData)
+                           );
+                        } else {
+                           formula = formula.replace(
+                              match,
+                              sourceName == "startData"
+                                 ? startData[fieldName]
+                                 : previousData[fieldName]
+                           );
+                        }
+                     }
+                     // Pull data from the repeat data
+                     else if (sourceName == "repeatData") {
+                        let fieldRepeat = this.fieldRepeat;
+                        if (fieldRepeat || fieldRepeat.datasourceLink) {
+                           formula = formula.replace(
+                              match,
+                              getFieldValue(
+                                 fieldRepeat.datasourceLink,
+                                 fieldName,
+                                 rawData
+                              )
+                           );
+                        }
+                     }
+                     // Pull data from a saved parameter in the query task
+                     else {
+                        let processField = (
+                           this.process.processDataFields(this) || []
+                        ).filter(
+                           (opt) =>
+                              opt &&
+                              (opt.key == param ||
+                                 opt.value == param ||
+                                 opt.label == param)
+                        )[0];
+
+                        if (processField) {
+                           let processData = this.process.processData(this, [
+                              instance,
+                              processField.key
+                           ]);
+
+                           if (Array.isArray(processData))
+                              processData = processData.filter(
+                                 (d) => d != null
+                              );
+
+                           formula = formula.replace(match, processData);
+                        }
+                     }
+                  });
+
+                  let evalValue = eval(formula);
 
                   if (
                      evalValue.toString &&
@@ -265,6 +375,65 @@ module.exports = class InsertRecord extends InsertRecordTaskCore {
                   item.value,
                   rawData
                );
+               break;
+            case "6":
+               var paramKeys = (item.value || "").split(",");
+               (paramKeys || []).forEach((key) => {
+                  if (key == null) return;
+
+                  let processData = this.process.processData(this, [
+                     instance,
+                     key
+                  ]);
+                  if (processData == null) {
+                     result[field.columnName] =
+                        result[field.columnName] != null &&
+                        result[field.columnName] != ""
+                           ? result[field.columnName]
+                           : null;
+                     return;
+                  }
+
+                  // If .field is a connect field who has M:1 or M:N relations, then it will set value with an array
+                  let isMultipleValue =
+                     field.key == "connectObject" &&
+                     field.settings &&
+                     field.settings.linkType == "many";
+                  if (isMultipleValue) {
+                     result[field.columnName] = result[field.columnName] || [];
+
+                     // Reformat processData to be M:1 connect data value
+                     let data = [];
+                     if (data == null) {
+                        data = [];
+                     } else if (Array.isArray(processData)) {
+                        data = processData.filter((d) => d != null);
+                     } else if (
+                        typeof processData == "string" ||
+                        typeof processData == "number"
+                     ) {
+                        data = {};
+                        data["id"] = processData;
+                        data["uuid"] = processData;
+                     } else {
+                        data = processData;
+                     }
+
+                     result[field.columnName] = result[field.columnName].concat(
+                        data
+                     );
+                  }
+                  // If .field supports a single value, then it pull only the first value item.
+                  else if (
+                     result[field.columnName] == null ||
+                     result[field.columnName] == ""
+                  ) {
+                     result[field.columnName] =
+                        (Array.isArray(processData)
+                           ? processData.filter((d) => d != null)[0]
+                           : processData) || null;
+                  }
+               });
                break;
          }
       });
