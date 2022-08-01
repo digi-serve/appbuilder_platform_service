31068071cef730f725fbe3b0de2da54bb44a03cc
Nate <7259@pm.me>
2022-08-01 12:10:19 +0700

    update core

--- platform/dataFields/ABFieldSelectivity.js
+++ platform/dataFields/ABFieldSelectivity.js
@@ -1,3 +1,8 @@
 const ABField = require("./ABField");
 
-module.exports = class ABFieldSelectivity extends ABField {};
+module.exports = class ABFieldSelectivity extends ABField {
+   static defaults() {
+      // We need a defaults fn for core, see https://github.com/digi-serve/appbuilder_class_core/pull/83
+      return {};
+   }
+};
