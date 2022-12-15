78ac0ad0f6b6f263c06dd1ec047aa3740a4fd4b4
Nate <7259@pm.me>
2022-12-15 11:04:11 +0700

    Add an optional instance key to prevent duplicate process triggers

--- platform/process/tasks/ABProcessTrigger.js
+++ platform/process/tasks/ABProcessTrigger.js
@@ -3,7 +3,7 @@ const path = require("path");
 const ABProcessTriggerCore = require(path.join(__dirname, "..", "..", "..", "core", "process", "tasks", "ABProcessTriggerCore.js"));
 
 module.exports = class ABProcessTaskTrigger extends ABProcessTriggerCore {
-   trigger(data, req) {
+   trigger(data, req, instanceKey) {
       // call my process.newInstance with
       if (!this.process) {
          var error = new Error("ABProcessTaskTrigger with a missing process?");
@@ -15,8 +15,15 @@ module.exports = class ABProcessTaskTrigger extends ABProcessTriggerCore {
       context.startTaskID = this.diagramID;
 
       return Promise.resolve()
-         .then(() => this.process.instanceNew(context, null, req))
+         .then(() => this.process.instanceNew(context, null, req, instanceKey))
          .catch((error) => {
+            if (error.nativeError.code == "ER_DUP_ENTRY") {
+               // This means the instanceKey already exisits in the database,
+               // which can happen if the first request times out. We want
+               // to report this as a sucess, so the retries stop.
+               req.log(`Process already trigger for ${instanceKey}`);
+               return;
+            }
             this.AB.notify.developer(error, {
                context: `ABProcessTrigger.trigger()`,
                task: this,
