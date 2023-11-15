d0dd84faca5da86a110dadde97cd0621024756b8
Johnny Hausman <johnny@hausmanfamily.com>
2023-11-15 15:11:17 -0600

    [add] include ABMobileViewLabel

---
+++ platform/mobile/ABMobileViewLabel.js
@@ -0,0 +1,17 @@
+const ABMobileViewLabelCore = require("../../core/mobile/ABMobileViewLabelCore");
+
+module.exports = class ABMobileViewLabel extends ABMobileViewLabelCore {
+   // constructor(values, application, parent, defaultValues) {
+   //    super(values, application, parent, defaultValues);
+   // }
+   // warningsEval() {
+   //    super.warningsEval();
+   //    let allViews = this.views();
+   //    if (allViews.length == 0) {
+   //       this.warningsMessage("has no sub views");
+   //    }
+   //    (this.pages() || []).forEach((p) => {
+   //       p.warningsEval();
+   //    });
+   // }
+};
