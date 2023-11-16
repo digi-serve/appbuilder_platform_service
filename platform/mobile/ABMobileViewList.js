271c07e46e0aef095e29377ddb5a20fbc2f7dd78
Johnny Hausman <johnny@hausmanfamily.com>
2023-11-15 18:09:36 -0600

    [add] Mobile List widget

---
+++ platform/mobile/ABMobileViewList.js
@@ -0,0 +1,17 @@
+const ABMobileViewListCore = require("../../core/mobile/ABMobileViewListCore");
+
+module.exports = class ABMobileViewList extends ABMobileViewListCore {
+   // constructor(values, application, parent, defaultValues) {
+   //    super(values, application, parent, defaultValues);
+   // }
+   // warningsEval() {
+   //    super.warningsEval();
+   //    let allViews = this.views();
+   //    if (allViews.length == 0) {
+   //       this.warningsMessage("has no sub views");
+   //    }
+   //    (this.pages() || []).forEach((p) => {
+   //       p.warningsEval();
+   //    });
+   // }
+};
