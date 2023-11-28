6da8b671d1fcc3081cc39d4f5d441b0fd6d34b7f
Johnny Hausman <johnny@hausmanfamily.com>
2023-11-28 12:52:05 -0600

    [wip] adding Mobile Forms

---
+++ platform/mobile/ABMobileViewForm.js
@@ -0,0 +1,17 @@
+const ABMobileViewFormCore = require("../../core/mobile/ABMobileViewFormCore");
+
+module.exports = class ABMobileViewForm extends ABMobileViewFormCore {
+   // constructor(values, application, parent, defaultValues) {
+   //    super(values, application, parent, defaultValues);
+   // }
+   // warningsEval() {
+   //    super.warningsEval();
+   //    let allViews = this.views();
+   //    if (allViews.length == 0) {
+   //       this.warningsMessage("has no sub views");
+   //    }
+   //    (this.pages() || []).forEach((p) => {
+   //       p.warningsEval();
+   //    });
+   // }
+};
---
+++ rules/ABViewRuleListFormRecordRules.js
@@ -0,0 +1,21 @@
+/**
+ * @Class ABViewRuleListFormRecordRules
+ * On the ab_platform_service platform, the ABRecordRule is just
+ * a blank stub. These only operate on the WEB platform, but are
+ * referenced by our FORM components.
+ */
+module.exports = class ABViewRuleListFormRecordRules {
+   // constructor(values, application, parent, defaultValues) {
+   //    super(values, application, parent, defaultValues);
+   // }
+   // warningsEval() {
+   //    super.warningsEval();
+   //    let allViews = this.views();
+   //    if (allViews.length == 0) {
+   //       this.warningsMessage("has no sub views");
+   //    }
+   //    (this.pages() || []).forEach((p) => {
+   //       p.warningsEval();
+   //    });
+   // }
+};
