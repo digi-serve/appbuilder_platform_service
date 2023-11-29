86a43dcef7fb5d8eb2e3bc2afdd73a13aa7f7f84
Johnny Hausman <johnny@hausmanfamily.com>
2023-11-29 12:18:34 -0600

    [wip] adding Form Textbox and Buttons

---
+++ platform/mobile/ABMobileViewFormButton.js
@@ -0,0 +1,19 @@
+const ABMobileViewFormButtonCore = require("../../core/mobile/ABMobileViewFormButtonCore");
+
+module.exports = class ABMobileViewFormButton extends (
+   ABMobileViewFormButtonCore
+) {
+   // constructor(values, application, parent, defaultValues) {
+   //    super(values, application, parent, defaultValues);
+   // }
+   // warningsEval() {
+   //    super.warningsEval();
+   //    let allViews = this.views();
+   //    if (allViews.length == 0) {
+   //       this.warningsMessage("has no sub views");
+   //    }
+   //    (this.pages() || []).forEach((p) => {
+   //       p.warningsEval();
+   //    });
+   // }
+};
