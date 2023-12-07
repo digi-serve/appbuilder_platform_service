95a70b735ad3f5b04ce7c08c5ca953e26be95a76
Johnny Hausman <johnny@hausmanfamily.com>
2023-12-07 13:35:42 -0600

    [wip] adding Mobile Form File  Formula  Image

---
+++ platform/mobile/ABMobileViewFormFile.js
@@ -0,0 +1,17 @@
+const ABMobileViewFormFileCore = require("../../core/mobile/ABMobileViewFormFileCore");
+
+module.exports = class ABMobileViewFormFile extends ABMobileViewFormFileCore {
+   // constructor(values, application, parent, defaultValues) {
+   //    super(values, application, parent, defaultValues);
+   // }
+   // warningsEval() {
+   //    super.warningsEval();
+   //    let allViews = this.views();
+   //    if (allViews.length == 0) {
+   //       this.warningsMessage("has no sub views");
+   //    }
+   //    (this.pages() || []).forEach((p) => {
+   //       p.warningsEval();
+   //    });
+   // }
+};
---
+++ platform/mobile/ABMobileViewFormImage.js
@@ -0,0 +1,17 @@
+const ABMobileViewFormImageCore = require("../../core/mobile/ABMobileViewFormImageCore");
+
+module.exports = class ABMobileViewFormImage extends ABMobileViewFormImageCore {
+   // constructor(values, application, parent, defaultValues) {
+   //    super(values, application, parent, defaultValues);
+   // }
+   // warningsEval() {
+   //    super.warningsEval();
+   //    let allViews = this.views();
+   //    if (allViews.length == 0) {
+   //       this.warningsMessage("has no sub views");
+   //    }
+   //    (this.pages() || []).forEach((p) => {
+   //       p.warningsEval();
+   //    });
+   // }
+};
