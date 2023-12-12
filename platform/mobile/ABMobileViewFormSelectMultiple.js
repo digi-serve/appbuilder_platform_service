9e48f9acf1466a0a97ecb6100af762d8bed7ed27
Johnny Hausman <johnny@hausmanfamily.com>
2023-12-12 15:58:52 -0600

    [wip] adding Mobile Form Number SelectMultiple  SelectSingle

---
+++ platform/mobile/ABMobileViewFormSelectMultiple.js
@@ -0,0 +1,19 @@
+const ABMobileViewFormSelectMultipleCore = require("../../core/mobile/ABMobileViewFormSelectMultipleCore");
+
+module.exports = class ABMobileViewFormSelectMultiple extends (
+   ABMobileViewFormSelectMultipleCore
+) {
+   // constructor(values, application, parent, defaultValues) {
+   //    super(values, application, parent, defaultValues);
+   // }
+   // warningsEval() {
+   //    super.warningsEval();
+   //    let allViews = this.views();
+   //    if (allViews.length == 0) {
+   //       this.warningsMessage("has no sub views");
+   //    }
+   //    (this.pages() || []).forEach((p) => {
+   //       p.warningsEval();
+   //    });
+   // }
+};
---
+++ platform/mobile/ABMobileViewFormSelectSingle.js
@@ -0,0 +1,19 @@
+const ABMobileViewFormSelectSingleCore = require("../../core/mobile/ABMobileViewFormSelectSingleCore");
+
+module.exports = class ABMobileViewFormSelectSingle extends (
+   ABMobileViewFormSelectSingleCore
+) {
+   // constructor(values, application, parent, defaultValues) {
+   //    super(values, application, parent, defaultValues);
+   // }
+   // warningsEval() {
+   //    super.warningsEval();
+   //    let allViews = this.views();
+   //    if (allViews.length == 0) {
+   //       this.warningsMessage("has no sub views");
+   //    }
+   //    (this.pages() || []).forEach((p) => {
+   //       p.warningsEval();
+   //    });
+   // }
+};
